0f4f38568325dd180e44907eeeabfa19a9f85332
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2022-07-24 03:52:15 +0530

    contracts updated to burn & mint LP tokens

--- contract/contracts/NERC233Swap.sol
+++ contract/contracts/NERC233Swap.sol
@@ -7,12 +7,15 @@ import "./NERC233Helper.sol";
 
 contract NERC233Swap is NERC233ContractReceiver {
     NERC233Helper private helper;
+    string private tokenSymbolLP;
+
     string public poolName;
     mapping(address => mapping(string => uint)) private deposits; // USER_ADDR >> SYMBOL >> AMOUNT
 
-    constructor(string memory _poolName, address _helperAddress) {
+    constructor(string memory _poolName, address _helperAddress, string memory _tokenSymbolLP) {
         poolName = _poolName;
         helper = NERC233Helper(_helperAddress);
+        tokenSymbolLP = _tokenSymbolLP;
     }
 
     function getDeposits(address _user, string calldata _symbol) public view returns(uint) {
@@ -26,21 +29,35 @@ contract NERC233Swap is NERC233ContractReceiver {
         depositToken(from, value, tokenAddress);
     }
 
+    // user sending money to deposit to contract initiates a callback
+    // contract registers deposit into user's account
+    // contract mints LP token to user.
     function depositToken(address _from, uint _value, address _tokenAddress) private {
+        // deposit to contract (++ valueIn)
         string memory _symbol = helper.getTokenSymbol(_tokenAddress);
         deposits[_from][_symbol] += _value;
+        // mint LP token to user
+        helper.mintToken(_from, tokenSymbolLP, _value);
     }
 
+    // withdrawing: user has to approve contract to use `value` worth LP tokens
+    // contract can send user `value` worth deposited symbol
+    // contract burns LP token from user
     function withdrawToken(string calldata _symbol, uint _value) public {
+        address payable _tokenAddressLP = payable(helper.getTokenAddress(tokenSymbolLP));
+        NERC233 tokenLP = NERC233(_tokenAddressLP);
         // checks
         require(helper.getTokenBalance(address(this), _symbol) > _value);
         require(deposits[msg.sender][_symbol] >= _value);
+        require(tokenLP.allowance(msg.sender, address(this)) >= _value);
         // update local values
         deposits[msg.sender][_symbol] -= _value;
         // transfer out to user
         address payable _tokenAddress = payable(helper.getTokenAddress(_symbol));
         NERC233 token = NERC233(_tokenAddress);
         token.transfer(msg.sender, _value);
+        // burn LP token from user
+        helper.burnToken(msg.sender, tokenSymbolLP, _value);
     }
 
     // user has to approve contract to pull `valueIn`
